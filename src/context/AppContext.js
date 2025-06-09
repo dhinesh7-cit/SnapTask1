536fed906e1c507cb718d07bc5ed7d2ec495531e
Dhinesh Elangovan <mortimersmithgeez@gmail.com>
2025-06-09 19:54:22 +0530

    Add files via upload

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,16 @@
 
+import React, { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(null); // Global user state
+
+  return (
+    <AppContext.Provider value={{ user, setUser }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => useContext(AppContext);
